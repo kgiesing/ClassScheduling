d6ea7347f6a2c7bf56a123242ee3612f485f8245
Karl Giesing <karl.giesing001@umb.edu>
2014-04-27 03:06:46 -0400

    Issue #15: Courses sort by enrollment
    Prior to this commit, Course class sorted only on ID, and not
    enrollment.

--- src/Course.cpp
+++ src/Course.cpp
@@ -9,6 +9,15 @@ Course::Course(const std::string& id, const std::string& name, int enrolled,
 
 int Course::compare(const Course& rhs) const
 {
-    // Sort on ID's only
-    return _id.compare(rhs._id);
+    // Check ID's
+    if (_id.compare(rhs._id) == 0)
+        return 0;
+    // ID's don't match; does enrollment?
+    if (_enrolled == rhs._enrolled)
+    {
+        // Different rooms with same enrollment; compare ID's
+        return _id.compare(rhs._id);
+    }
+    // Enrollment doesn't match
+    return _enrolled - rhs._enrolled;
 }
