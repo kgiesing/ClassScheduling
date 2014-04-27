8dc59b49e456b7455a48b288976d09a9962d161d
Karl Giesing <karl.giesing001@umb.edu>
2014-04-26 20:18:34 -0400

    Issue #11: Added "void" to void methods
    Code auto-generated by dia2code does not put "void" in front of void
    methods.

--- include/ScheduleWriter.h
+++ include/ScheduleWriter.h
@@ -7,8 +7,8 @@
 class ScheduleWriter : public FileWriter<Schedule> {
 public:
     ScheduleWriter(std::string delimiter = "\t");
-    setFieldDelimiter(std::string delimiter);
-    setContents(Schedule contents);
+    void setFieldDelimiter(std::string delimiter);
+    void setContents(Schedule contents);
 private:
     std::string _delimiter;
 };
