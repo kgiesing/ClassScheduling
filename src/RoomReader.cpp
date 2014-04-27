72ae648f8f51cd890d5cab124703c63e64fae90c
Karl Giesing <karl.giesing001@umb.edu>
2014-04-26 21:46:31 -0400

    Issue #11: Implemented RoomReader

---
+++ src/RoomReader.cpp
@@ -0,0 +1,35 @@
+#include "../include/RoomReader.h"
+#include <fstream>
+#include <set>
+
+using std::ifstream;
+using std::set;
+using std::string;
+using std::vector;
+
+vector<Room> RoomReader::read()
+{
+    ifstream file;
+    string id;
+    string temp;
+    int capacity;
+    set<Room> rooms;
+
+    file.open(this->getFilename().c_str());
+    while (!file.eof())
+    {
+        getline(file, id, this->getDelimiter());
+        // Stream capacity as int, then discard chars before newline
+        file >> capacity;
+        getline(file, temp);
+        // Construct the object, add it to the set
+        Room r(id, capacity);
+        rooms.insert(r);
+    }
+    file.close();
+
+    // Convert to vector and return
+    vector<Room> output(profs.begin(), profs.end());
+    return output;
+}
+
