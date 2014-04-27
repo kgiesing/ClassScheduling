3328eee20c5d7bb6abc0f9105e2047bb9c494162
Karl Giesing <karl.giesing001@umb.edu>
2014-04-27 17:58:56 -0400

    Issue #16: Added getters/setters to ProfInfo
    We should retrieve score, Weekdays, and TimeBlock separately from info;
    added getters and setters for each.

--- include/ProfInfo.h
+++ include/ProfInfo.h
@@ -17,6 +17,17 @@ public:
      */
     ProfInfo (const Prof&, Weekdays = WEEKDAYS_SIZE, TimeBlock = TIMEBLOCK_SIZE);
     /**
+     * Adds a time that the professor is teaching a course.
+     * @param The weekday on which the course is taught.
+     * @param The time block when the course is taught.
+     */
+    void addTime(Weekdays w, TimeBlock t);
+    /**
+     * Returns the last TimeBlock found.
+     * @return The last TimeBlock found.
+     */
+    Weekdays getDay() const { return _lastWeekday; }
+    /**
      * Returns the professor about which we are holding information.
      * @return Professor about which we are holding information.
      */
@@ -27,11 +38,15 @@ public:
      */
     double getScore() const { return _score; }
     /**
-     * Adds a time that the professor is teaching a course.
-     * @param The weekday on which the course is taught.
-     * @param The time block when the course is taught.
+     * Returns the last TimeBlock found.
+     * @return The last TimeBlock found.
      */
-    void addTime(Weekdays w, TimeBlock t);
+    TimeBlock getTime() const { return _lastTimeBlock; }
+    /**
+     * Sets the professor's current score.
+     * @param The professor's current score.
+     */
+    void setScore(double score) { _score = score; }
 private:
     const Prof& _prof;
     Weekdays _lastWeekday;
