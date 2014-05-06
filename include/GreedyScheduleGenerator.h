7d8d11a39710117144045c83f7e6417d27351108
linsiyu1992 <l.fish@126.com>
2014-05-06 10:20:09 -0400

    update GreedyScheduleGenerator.h

--- include/GreedyScheduleGenerator.h
+++ include/GreedyScheduleGenerator.h
@@ -11,10 +11,16 @@ using namespace std;
 
 class GreedyScheduleGenerator : public ScheduleGenerator {
 public:
-  Schedule* getSchedule(void);
+	Schedule* getSchedule(void);
+	GreedyScheduleGenerator(vector<Room>& roomV, vector<Prof>& profV, vector<Course>& courseV, ScoreCalculator& sc);
 private:
-  ScoreCalculator& _sc;
-  map<string, double> _scores;
+	ScoreCalculator& _sc;
+	map<string, double> _scores;
+
+	static const int AVAILABLE_MON_WED = 1;
+	static const int AVAILABLE_TUE_THU = 2;
+	static const int NOT_AVAILABLE = 0;
+	int getAvailableWeekday(Schedule& schedule, Room& room, TimeBlock time);
 };
 
 #endif // GREEDYSCHEDULEGENERATOR_H
