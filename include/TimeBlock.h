dfae0e6011d51b5175ce95e457d196c948ac769a
Karl Giesing <karl.giesing001@umb.edu>
2014-05-07 23:12:50 -0400

    Restored TimeBlock.h
    Somehow, this got deleted when the test files were moved to the
    testsuite subfolder. Restoring.

---
+++ include/TimeBlock.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef TIMEBLOCK_H
+#define TIMEBLOCK_H
+
+/**
+ * Enumeration of daily time blocks. The final value is a "dummy value"
+ * that represents the "size" of the enum; it is for iterating through the
+ * enum's values. (This trick only works with continuous enum values.)
+ */
+enum TimeBlock {
+  START_08_00,
+  START_09_30,
+  START_11_00,
+  START_12_30,
+  START_14_00,
+  START_15_30,
+  START_17_00,
+  START_18_30,
+  START_20_00,
+  TIMEBLOCK_SIZE /**< Dummy value for iterating through enum */
+};
+
+/**
+ * Overwrite stream output operator
+ */
+std::ostream& operator<<(std::ostream& out, const TimeBlock value);
+
+#endif // TIMEBLOCK_H
