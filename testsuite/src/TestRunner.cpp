393638b9784d1fa3777e7c0cad8e793004b6e928
Karl Giesing <karl.giesing001@umb.edu>
2014-05-08 13:14:44 -0400

    Issue #9: Fixed output bug
    Forgot to put a space between the component name and the word
    "complete." Oops.

--- testsuite/src/TestRunner.cpp
+++ testsuite/src/TestRunner.cpp
@@ -6,7 +6,7 @@ void TestRunner::runPassTests(void)
     {
         cout << "Running pass tests for " << this->getComponent() << "...\n";
         this->testPass();
-        cout << "Pass tests for " << this->getComponent() << "complete.\n\n";
+        cout << "Pass tests for " << this->getComponent() << " complete.\n\n";
     }
     catch (...)
     {
@@ -20,7 +20,7 @@ void TestRunner::runFailTests(void)
     {
         cout << "Running fail tests for " << this->getComponent() << "...\n";
         this->testFail();
-        cout << "Fail tests for " << this->getComponent() << "complete.\n\n";
+        cout << "Fail tests for " << this->getComponent() << " complete.\n\n";
     }
     catch (...)
     {
