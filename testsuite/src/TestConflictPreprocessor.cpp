78e707584fe72536cd476ed56a9458062c281efb
Karl Giesing <karl.giesing001@umb.edu>
2014-05-10 13:49:10 -0400

    Issue #9: Pass tests for TestConflictPreprocessor
    Actually implemented the tests.

--- testsuite/src/TestConflictPreprocessor.cpp
+++ testsuite/src/TestConflictPreprocessor.cpp
@@ -1,5 +1,6 @@
 #include "../include/TestConflictPreprocessor.h"
 #include "../include/DataCreator.h"
+#include "../include/VectorPrinter.h"
 #include "../../include/ConflictPreprocessor.h"
 
 //* Implement as class is finished
@@ -9,7 +10,16 @@ void TestConflictPreprocessor::testPass(void)
     Course c;
     vector<Course> courses = DataCreator::createVector(c);
 
+    // Output values before preprocessing
+    cout << "\nVector before preprocessing: " << endl;
+    VectorPrinter::print(courses);
+
     // Call the static method
     ConflictPreprocessor::preprocess(courses);
+    cout << "ConflictPreprocessor::preprocess() successfully run." << endl;
+
+    // Output values before preprocessing
+    cout << "Vector after preprocessing: " << endl;
+    VectorPrinter::print(courses);
 }
 
