81d8dc712070d22b40406bddc313c0ab337cc145
Karl Giesing <karl.giesing001@umb.edu>
2014-05-11 14:22:42 -0400

    Fixed #51.

--- testsuite/src/TestSchedule.cpp
+++ testsuite/src/TestSchedule.cpp
@@ -10,51 +10,57 @@ void TestSchedule::testPass(void)
     // Create valid sample data for initialization
     vector<Prof> profs = DataCreator::createVector(DataCreator::createProf());
     vector<Room> rooms = DataCreator::createVector(DataCreator::createRoom());
-    Course c1 = DataCreator::createCourse();
-    c1.setProfId(profs[0].getId());
-    Course c2 = DataCreator::createCourse();
-    c2.setProfId(profs[1].getId());
+    vector<Course> courses = DataCreator::createVector(DataCreator::createCourse());
+    DataCreator::loadProfs(courses, profs);
 
     // Instantiate new Schedule
-    instance = new Schedule(rooms, profs);
+    instance = new Schedule(rooms, profs, courses);
     cout << "Successfully created Prof object with data" << endl;
-    cout << "Test courses passed as arguments:" << endl;
-    cout << "\tc1: " << endl;
-    ObjectPrinter::print(c1, "\t   ");
-    cout << "\tc2: " << endl;
-    ObjectPrinter::print(c2, "\t   ");
+
+    // Choose a course as a test parameter
+    Course c  = courses[10];
+    Course c2 = courses[5];
+    cout << "Course objects passed as parameter:" << endl
+         << "\tc: ";
+    ObjectPrinter::print(c, "\t    ");
+    cout << "\tc2: ";
+    ObjectPrinter::print(c2, "\t    ");
 
     // Test accessors
-    cout << "Testing accessors..." << endl;
+    cout << "Object after mutating:" << endl;
     cout << "\tgetCourse(rooms[0], MON, START_08_00): " << endl;
     ObjectPrinter::print(instance->getCourse(rooms[0], MON, START_08_00), "\t");
+    cout << "\tgetCourse(rooms[0], TUES, START_14_00): " << endl;
+    ObjectPrinter::print(instance->getCourse(rooms[0], TUES, START_14_00), "\t");
     cout << "\tgetCoursesAt(MON, START_08_00): " << endl;
     VectorPrinter::print(instance->getCoursesAt(MON, START_08_00), "\t");
-    cout << "\tgetProf(c1): " << endl;
-    ObjectPrinter::print(instance->getProf(c1), "\t");
+    cout << "\tgetCoursesAt(TUES, START_14_00): " << endl;
+    VectorPrinter::print(instance->getCoursesAt(TUES, START_14_00), "\t");
+    cout << "\tgetProf(c): " << endl;
+    ObjectPrinter::print(instance->getProf(c), "\t");
     cout << "\tgetProf(c2): " << endl;
-    ObjectPrinter::print(instance->getProf(c1), "\t");
+    ObjectPrinter::print(instance->getProf(c2), "\t");
     cout << "\tgetProf(\"" << profs[0].getId() << "\"): " << endl;
     ObjectPrinter::print(instance->getProf(profs[0].getId()), "\t");
-    cout << "\tgetRoomFor(c1): " << endl;
-    ObjectPrinter::print(instance->getRoomFor(c1), "\t");
+    cout << "\tgetRoomFor(c): " << endl;
+    ObjectPrinter::print(instance->getRoomFor(c), "\t");
     cout << "\tgetRoomFor(c2): " << endl;
-    ObjectPrinter::print(instance->getRoomFor(c2), "\t");
+    ObjectPrinter::print(instance->getRoomFor(c), "\t");
     cout << "\tgetScore(): " << endl;
     cout << "\t" << instance->getScore() << endl;
-    cout << "\tgetTimeFor(c1): " << endl;
-    ObjectPrinter::print(instance->getTimeFor(c2), "\t");
+    cout << "\tgetTimeFor(c): " << endl;
+    ObjectPrinter::print(instance->getTimeFor(c), "\t");
     cout << "\tgetTimeFor(c2): " << endl;
     ObjectPrinter::print(instance->getTimeFor(c2), "\t");
-    cout << "\tgetWeekdaysFor(c1): " << endl;
-    VectorPrinter::print(instance->getWeekdaysFor(c1), "\t");
+    cout << "\tgetWeekdaysFor(c): " << endl;
+    VectorPrinter::print(instance->getWeekdaysFor(c), "\t");
     cout << "\tgetWeekdaysFor(c2): " << endl;
     VectorPrinter::print(instance->getWeekdaysFor(c2), "\t");
 
     // Test mutators
     cout << "Testing mutators..." << endl;
-    cout << "\tsetCourse(c1, rooms[0], MON, START_08_00, 2): "
-         << instance->setCourse(c1, rooms[0], MON, START_08_00, 2) << endl;
+    cout << "\tsetCourse(c, rooms[0], MON, START_08_00, 2): "
+         << instance->setCourse(c, rooms[0], MON, START_08_00, 2) << endl;
     cout << "\tsetCourse(c2, rooms[1], TUES, START_14_00, 2): "
          << instance->setCourse(c2, rooms[1], TUES, START_14_00, 2) << endl;
     cout << "\tsetScore(100.0)..." << endl;
@@ -68,26 +74,30 @@ void TestSchedule::testPass(void)
     cout << "Object after mutating:" << endl;
     cout << "\tgetCourse(rooms[0], MON, START_08_00): " << endl;
     ObjectPrinter::print(instance->getCourse(rooms[0], MON, START_08_00), "\t");
+    cout << "\tgetCourse(rooms[0], TUES, START_14_00): " << endl;
+    ObjectPrinter::print(instance->getCourse(rooms[0], TUES, START_14_00), "\t");
     cout << "\tgetCoursesAt(MON, START_08_00): " << endl;
     VectorPrinter::print(instance->getCoursesAt(MON, START_08_00), "\t");
-    cout << "\tgetProf(c1): " << endl;
-    ObjectPrinter::print(instance->getProf(c1), "\t");
+    cout << "\tgetCoursesAt(TUES, START_14_00): " << endl;
+    VectorPrinter::print(instance->getCoursesAt(TUES, START_14_00), "\t");
+    cout << "\tgetProf(c): " << endl;
+    ObjectPrinter::print(instance->getProf(c), "\t");
     cout << "\tgetProf(c2): " << endl;
-    ObjectPrinter::print(instance->getProf(c1), "\t");
+    ObjectPrinter::print(instance->getProf(c2), "\t");
     cout << "\tgetProf(\"" << profs[0].getId() << "\"): " << endl;
     ObjectPrinter::print(instance->getProf(profs[0].getId()), "\t");
-    cout << "\tgetRoomFor(c1): " << endl;
-    ObjectPrinter::print(instance->getRoomFor(c1), "\t");
+    cout << "\tgetRoomFor(c): " << endl;
+    ObjectPrinter::print(instance->getRoomFor(c), "\t");
     cout << "\tgetRoomFor(c2): " << endl;
-    ObjectPrinter::print(instance->getRoomFor(c2), "\t");
+    ObjectPrinter::print(instance->getRoomFor(c), "\t");
     cout << "\tgetScore(): " << endl;
     cout << "\t" << instance->getScore() << endl;
-    cout << "\tgetTimeFor(c1): " << endl;
-    ObjectPrinter::print(instance->getTimeFor(c2), "\t");
+    cout << "\tgetTimeFor(c): " << endl;
+    ObjectPrinter::print(instance->getTimeFor(c), "\t");
     cout << "\tgetTimeFor(c2): " << endl;
     ObjectPrinter::print(instance->getTimeFor(c2), "\t");
-    cout << "\tgetWeekdaysFor(c1): " << endl;
-    VectorPrinter::print(instance->getWeekdaysFor(c1), "\t");
+    cout << "\tgetWeekdaysFor(c): " << endl;
+    VectorPrinter::print(instance->getWeekdaysFor(c), "\t");
     cout << "\tgetWeekdaysFor(c2): " << endl;
     VectorPrinter::print(instance->getWeekdaysFor(c2), "\t");
 
