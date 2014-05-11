93d80ba9012a993c87f0ec93f600a60a2caa9878
Karl Giesing <karl.giesing001@umb.edu>
2014-05-10 22:43:29 -0400

    Issue #9: Invalid files created at runtime
    They are put into res/<type>_fail.txt.
    
    - Creates 50 random courses, many of which are likely duplicates, with
    conflicts that may not exist
    - Creates 10 random professors
    - Creates 5 random classrooms

--- testsuite/src/main.cpp
+++ testsuite/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "../include/DataCreator.h"
 #include "../include/TestConflictPreprocessor.h"
 #include "../include/TestFileReaderFactory.h"
 #include "../include/TestGeneticScheduleGenerator.h"
@@ -13,13 +14,20 @@ using namespace std;
 const string VALID_COURSES = "./res/courses_ok.txt";
 const string VALID_PROFS   = "./res/profs_ok.txt";
 const string VALID_ROOMS   = "./res/rooms_ok.txt";
-// TODO: Create these
-const string INVALID_COURSES = "./res/courses_ok.txt";
-const string INVALID_PROFS   = "./res/profs_ok.txt";
-const string INVALID_ROOMS   = "./res/rooms_ok.txt";
+const string INVALID_COURSES = "./res/courses_fail.txt";
+const string INVALID_PROFS   = "./res/profs_fail.txt";
+const string INVALID_ROOMS   = "./res/rooms_fail.txt";
 
 int main()
 {
+    // Dynamically create invalid data
+    DataCreator::createVectorFile(INVALID_COURSES,
+                                  DataCreator::createCourse(), 50);
+    DataCreator::createVectorFile(INVALID_PROFS,
+                                  DataCreator::createProf(), 10);
+    DataCreator::createVectorFile(INVALID_ROOMS,
+                                  DataCreator::createRoom(), 5);
+
     TestRunner* instance;
 
     // Run tests on FileReaderFactory
