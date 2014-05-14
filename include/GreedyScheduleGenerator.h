dbf3e478021bf93669892071546e2dfc42be98c2
Karl Giesing <karl.giesing001@umb.edu>
2014-05-14 05:25:19 -0400

    Finished work; now must debug.

--- include/GreedyScheduleGenerator.h
+++ include/GreedyScheduleGenerator.h
@@ -3,21 +3,29 @@
 #define GREEDYSCHEDULEGENERATOR_H
 
 #include "ScheduleGenerator.h"
-#include "ScoreCalculator.h"
 #include <map>
 #include <string>
 
-using namespace std;
+using std::map;
+using std::string;
 
 class GreedyScheduleGenerator : public ScheduleGenerator {
 public:
-	GreedyScheduleGenerator(vector<Room>& roomV, vector<Prof>& profV, vector<Course>& courseV, long timeout) :
-		ScheduleGenerator(timeout), _rooms(roomV), _profs(profV), _courses(courseV) { };
+	GreedyScheduleGenerator(vector<Room>&, vector<Prof>&, vector<Course>&, long);
 	Schedule* getSchedule(void);
 private:
-    std::vector<Room>& _rooms;     /**< Vector of Room objects */
-    std::vector<Prof>& _profs;     /**< Vector of Prof objects */
-    std::vector<Course>& _courses; /**< Vector of Course objects */
+    // Private member variables:
+    std::vector<Room>& _rooms;
+    std::vector<Prof>& _profs;
+    std::vector<Course>& _courses;
+    vector<bool> _isScheduled;
+    vector<bool> _isFull;
+    map<string, unsigned> _courseMap;
+    const static Weekdays LAST_DAY = TUES;
+    Schedule* _schedule;
+    // Private member functions:
+    bool findNextTime(const Room, Weekdays&, TimeBlock&);
+    void scheduleConflict(unsigned, TimeBlock);
 };
 
 #endif // GREEDYSCHEDULEGENERATOR_H
--- src/GreedyScheduleGenerator.cpp
+++ src/GreedyScheduleGenerator.cpp
@@ -6,7 +6,145 @@
 #include "../include/TimeBlock.h"
 
 
-Schedule* GreedyScheduleGenerator::getSchedule() {
-	Schedule* schedule = new Schedule(_rooms, _profs, _courses);
+// DEBUG
+#include <iostream>
+using std::cout;
+using std::endl;
 
+GreedyScheduleGenerator::GreedyScheduleGenerator(vector<Room>& rooms,
+        vector<Prof>& profs, vector<Course>& courses, long timeout)
+    : ScheduleGenerator(timeout), _rooms(rooms), _profs(profs), _courses(courses),
+      _isScheduled(courses.size()), _isFull(rooms.size())
+{
+    // Create a map of course ID's to course indexes, for finding conflicts
+    for(unsigned i = 0; i < courses.size(); i++)
+        _courseMap[courses[i].getId()] = i;
+}
+
+
+Schedule* GreedyScheduleGenerator::getSchedule()
+{
+    //  Declare variables
+    TimeBlock time;
+    Weekdays day;
+    unsigned idxRoom;
+
+    // Create the (empty) Schedule object
+    _schedule = new Schedule(_rooms, _profs, _courses);
+
+    // Schedule courses by matching capacities to enrollment
+    idxRoom = 0;
+    day = MON;
+    time = START_08_00;
+    for(unsigned c = 0; c < _courses.size(); c++)
+    {
+        // DEBUG
+        cout << "\n\tScheduling course " << _courses[c].getId();
+
+        // Skip if it was scheduled as a conflict
+        if (_isScheduled[c])
+            continue;
+        _isFull[idxRoom] = findNextTime(_rooms[idxRoom], day, time);
+        // DEBUG
+        cout << "\n\tfrom findNextTime: " << day << time << ": " << _isFull[idxRoom];
+        while(_isFull[idxRoom] && idxRoom < _rooms.size())
+        {
+            // DEBUG
+            cout << "\n\tRollover: " << _isFull[idxRoom] << ", " << idxRoom;
+            time = START_08_00;
+            day = MON;
+            idxRoom++;
+            _isFull[idxRoom] = findNextTime(_rooms[idxRoom], day, time);
+        }
+        if (idxRoom == _rooms.size())
+        {
+            cout <<  "Cannot create valid schedule: all rooms full";
+            throw "Cannot create valid schedule: all rooms full";
+        }
+        if (_rooms[idxRoom].getCapacity() < _courses[c].getEnrolled())
+        {
+            cout <<  "Cannot create valid schedule: courses are too large";
+            throw "Cannot create valid schedule: courses are too large";
+        }
+        _schedule->setCourse(_courses[c], _rooms[idxRoom], day, time,
+                _courses[c].getTimeBlocks());
+        // Schedule the conflicting courses
+        set<string> conflicts = _courses[c].getConflicts();
+        for(set<string>::iterator it = conflicts.begin(); it != conflicts.end(); it++)
+            scheduleConflict(_courseMap[*it], time);
+        _isScheduled[c] = true;
+    }
+
+    // Done.
+    return _schedule;
+}
+
+bool GreedyScheduleGenerator::findNextTime(const Room r, Weekdays& day,
+        TimeBlock& time)
+{
+    // To reset day and time
+    Weekdays oldDay = day;
+    TimeBlock oldTime = time;
+
+    // DEBUG
+    cout << "\n\tfindNextTime: " << r.getId();
+
+    while(_schedule->getCourse(r, day, time).getId() != "")
+    {
+        // DEBUG
+        cout << "\n\t\t: " << day << time << "=\"" << _schedule->getCourse(r, day, time).getId() << "\"";
+
+        time = static_cast<TimeBlock>(time + 1);
+        if(time >= TIMEBLOCK_SIZE)
+        {
+            day = static_cast<Weekdays>(day + 1);
+            time = START_08_00;
+            if(day > LAST_DAY)
+            {
+                // No "next time" for this room...
+                day = oldDay;
+                time = oldTime;
+                // DEBUG
+                cout << "\n\t\t...rollover...";
+                return true;
+            }
+        }
+    }
+    // DEBUG
+    cout << "\n\tFound it : " << day << time << " " << _schedule->getCourse(r, day, time).getId();
+    return false;
+}
+
+void GreedyScheduleGenerator::scheduleConflict(unsigned c, TimeBlock time)
+{
+    // Starting at room with lowest possible capacity and work upwards
+    unsigned idxRoom = _rooms.size() - 1;
+    while(_rooms[idxRoom].getCapacity() >= _courses[c].getEnrolled())
+        idxRoom++;
+    idxRoom--; // Went past it; back up one
+    if(idxRoom < 0)
+        throw "Cannot create valid schedule: courses are too large";
+
+    // Schedule this course at a different time
+    TimeBlock tb = START_08_00;
+    Weekdays day = MON;
+
+    _isFull[idxRoom] = findNextTime(_rooms[idxRoom], day, tb);
+    while (tb == time || (_isFull[idxRoom] && idxRoom >= 0))
+    {
+        if(tb == time)  // Conflicts with original
+            tb = static_cast<TimeBlock>(tb + 1);
+        else // Room was full
+        {
+            time = START_08_00;
+            day = MON;
+            idxRoom--;
+        }
+        _isFull[idxRoom] = findNextTime(_rooms[idxRoom], day, time);
+    }
+    if (idxRoom < 0)
+        throw "Cannot create valid schedule: all rooms full";
+
+    _schedule->setCourse(_courses[c], _rooms[idxRoom], day, time,
+                         _courses[c].getTimeBlocks());
 }
