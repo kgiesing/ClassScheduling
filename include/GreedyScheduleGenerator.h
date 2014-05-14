b92e9292bc8b4e06c2722e63300d902cf65734ba
Karl Giesing <karl.giesing001@umb.edu>
2014-05-14 02:28:03 -0400

    Start work on greedy schedule generator.
    Since nobody else is going to...

--- include/GreedyScheduleGenerator.h
+++ include/GreedyScheduleGenerator.h
@@ -11,19 +11,13 @@ using namespace std;
 
 class GreedyScheduleGenerator : public ScheduleGenerator {
 public:
-	Schedule* getSchedule(void);
 	GreedyScheduleGenerator(vector<Room>& roomV, vector<Prof>& profV, vector<Course>& courseV, long timeout) :
-		ScheduleGenerator(timeout), _rooms(roomV), _profs(profV), _courses(courseV){};
+		ScheduleGenerator(timeout), _rooms(roomV), _profs(profV), _courses(courseV) { };
+	Schedule* getSchedule(void);
 private:
     std::vector<Room>& _rooms;     /**< Vector of Room objects */
     std::vector<Prof>& _profs;     /**< Vector of Prof objects */
     std::vector<Course>& _courses; /**< Vector of Course objects */
-
-	static const int AVAILABLE_MON_WED = 1;
-	static const int AVAILABLE_TUE_THU = 2;
-	static const int NOT_AVAILABLE = 0;
-	int getAvailableWeekday(Schedule& schedule, Room& room, TimeBlock time);
-	bool isConflict(Schedule& schedule, Weekdays day, TimeBlock time, Course& course);
 };
 
 #endif // GREEDYSCHEDULEGENERATOR_H
