667f18755af50c6d72441c8b1b6e54bd1c8d67c2
Karl Giesing <karl.giesing001@umb.edu>
2014-05-15 00:00:09 -0400

    Issue #9: Many improvements
    Created a MapPrinter, fixed import errors in VectorPrinter, made
    exception messages stand out more.

---
+++ testsuite/include/MapPrinter.h
@@ -0,0 +1,69 @@
+#ifndef MAPPRINTER_H
+#define MAPPRINTER_H
+
+#include "../../include/Course.h"
+#include "../../include/Prof.h"
+#include "../../include/ProfInfo.h"
+#include "../../include/Room.h"
+#include <iostream>
+#include <string>
+#include <map>
+
+using std::cout;
+using std::ostream;
+using std::string;
+using std::map;
+
+class MapPrinter
+{
+public:
+    /**
+     * Prints a map of string-to-Course objects to an output stream.
+     * @param std::map<string, Course> to print
+     * @param std::string to print before every new line
+     * @param std::ostream to output Course map; defaults to std::cout
+     */
+    static void print(const map<string, Course>&, string="", ostream& = cout);
+    /**
+     * Prints a map of string-to-Prof objects to an output stream.
+     * @param std::map<string, Course> to print
+     * @param std::string to print before every new line
+     * @param std::ostream to output Prof map; defaults to std::cout
+     */
+    static void print(const map<string, Prof>&, string="", ostream& = cout);
+    /**
+     * Prints a map of string-to-ProfInfo objects to an output stream.
+     * @param std::map<string, Course> to print
+     * @param std::string to print before every new line
+     * @param std::ostream to output Prof map; defaults to std::cout
+     */
+    static void print(const map<string, ProfInfo>&, string="", ostream& = cout);
+    /**
+     * Prints a map of string-to-Room objects to an output stream.
+     * @param std::map<string, Course> to print
+     * @param std::string to print before every new line
+     * @param std::ostream to output Room map; defaults to std::cout
+     */
+    static void print(const map<string, Room>&, string="", ostream& = cout);
+    /**
+     * Prints a map of string-to-TimeBlock enum values to an output stream.
+     * @param std::map<string, Course> to print
+     * @param std::string to print before every new line
+     * @param std::ostream to output TimeBlock enum values;
+     *        defaults to std::cout
+     */
+    static void print(const map<string, TimeBlock>&, string="", ostream& = cout);
+    /**
+     * Prints a map of string-to-Weekday enum values to an output stream.
+     * @param std::map<string, Course> print
+     * @param std::string to print before every new line
+     * @param std::ostream to output Weekday enum values;
+     *        defaults to std::cout
+     */
+    static void print(const map<string, Weekdays>&, string="", ostream& = cout);
+private:
+    /** Private constructor */
+    MapPrinter() { }
+};
+
+#endif // MAPPRINTER_H
---
+++ testsuite/src/MapPrinter.cpp
@@ -0,0 +1,79 @@
+#include "../include/MapPrinter.h"
+#include "../include/ObjectPrinter.h"
+
+using std::endl;
+
+void MapPrinter::print(const map<string, Course>& m, string prefix,
+                          ostream& out)
+{
+    map<string, Course>::const_iterator it;
+    out << "map<string -> Course>:" << endl;
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        out << prefix << "  \"" << it->first << "\"" << endl
+            << prefix << "  -> ";
+        ObjectPrinter::print(it->second, prefix, out);
+    }
+}
+
+void MapPrinter::print(const map<string, Prof>& m, string prefix,
+                          ostream& out)
+{
+    map<string, Prof>::const_iterator it;
+    out << "map<string -> Prof>:" << endl;
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        out << prefix << "  \"" << it->first << "\" -> ";
+        ObjectPrinter::print(it->second, prefix, out);
+    }
+}
+
+void MapPrinter::print(const map<string, ProfInfo>& m, string prefix,
+                          ostream& out)
+{
+    map<string, ProfInfo>::const_iterator it;
+    out << "map<string -> ProfInfo>:" << endl;
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        out << prefix << "  \"" << it->first << "\"" << endl
+            << prefix << "  -> ";
+        ObjectPrinter::print(it->second, prefix, out);
+    }
+}
+
+void MapPrinter::print(const map<string, Room>& m, string prefix,
+                          ostream& out)
+{
+    map<string, Room>::const_iterator it;
+    out << "map<string -> Room>:" << endl;
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        out << prefix << "  \"" << it->first << "\" -> ";
+        ObjectPrinter::print(it->second, prefix, out);
+    }
+}
+
+void MapPrinter::print(const map<string, TimeBlock>& m, string prefix,
+                          ostream& out)
+{
+    map<string, TimeBlock>::const_iterator it;
+    out << "map<string -> TimeBlock>:" << endl;
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        out << prefix << "  \"" << it->first << "\" -> ";
+        ObjectPrinter::print(it->second, prefix, out);
+    }
+}
+
+void MapPrinter::print(const map<string, Weekdays>& m, string prefix,
+                          ostream& out)
+{
+    map<string, Weekdays>::const_iterator it;
+    out << "map<string -> Weekdays>:" << endl;
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        out << prefix << "  \"" << it->first << "\" -> ";
+        ObjectPrinter::print(it->second, prefix, out);
+    }
+}
+
