eb6ae1f62cde83da37476db889c70f5b1a5133b5
Karl Giesing <karl.giesing001@umb.edu>
2014-05-17 01:53:47 -0400

    Issue #83: Stress tests for ScheduleCalculator
    This is the static class that runs the linear-time score calculations
    for a schedule.

--- testsuite/src/TestScheduleCalculator.cpp
+++ testsuite/src/TestScheduleCalculator.cpp
@@ -10,8 +10,8 @@ void TestScheduleCalculator::testPass(void)
 {
     // Create valid sample data for testing
     Schedule s = DataCreator::createSchedule();
-    ScoreCalculator* linear = new LinearScoreCalculator();
-    ScoreCalculator* weighted = new WeightedScoreCalculator();
+    LinearScoreCalculator linear;
+    WeightedScoreCalculator weighted;
 
     // Output sample data
     cout << "Test data: "
@@ -22,8 +22,47 @@ void TestScheduleCalculator::testPass(void)
     // Test static members
     cout << "Testing static members..." << endl;
     cout << "\tcalculateScore(s, linear): "
-         << ScheduleCalculator::calculateScore(&s, *linear) << endl;
+         << ScheduleCalculator::calculateScore(&s, linear) << endl;
     cout << "\tcalculateScore(s, weighted): "
-         << ScheduleCalculator::calculateScore(&s, *weighted) << endl;
+         << ScheduleCalculator::calculateScore(&s, weighted) << endl;
     cout << endl;
 }
+
+void TestScheduleCalculator::testStress(unsigned seconds)
+{
+
+    // Declare variables
+    time_t future;
+    unsigned long iterations;
+
+    // Create object
+    cout << "Creating objects and test data..." << endl;
+    LinearScoreCalculator linear;
+    WeightedScoreCalculator weighted;
+    Schedule s = DataCreator::createSchedule();
+
+    // Stress test WeightedScoreCalculator
+    cout << "Stress testing calculateScore(s, weighted)..." << endl;
+    iterations = 0;
+    future = time(NULL) + seconds;
+    while (time(NULL) < future)
+    {
+        ScheduleCalculator::calculateScore(&s, weighted);
+        iterations++;
+    }
+    cout << "\t" << iterations << " iterations in "
+         << seconds << " seconds" << endl;
+
+     // Stress test LinearScoreCalculator
+    cout << "Stress testing calculateScore(s, linear)..." << endl;
+    iterations = 0;
+    future = time(NULL) + seconds;
+    while (time(NULL) < future)
+    {
+        ScheduleCalculator::calculateScore(&s, linear);
+        iterations++;
+    }
+    cout << "\t" << iterations << " iterations in "
+         << seconds << " seconds" << endl;
+}
+
