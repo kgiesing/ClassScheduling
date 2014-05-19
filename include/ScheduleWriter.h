c82b5e41f0fdc9b8f9a9085bbc5c7f87875e9afe
Karl Giesing <karl.giesing001@umb.edu>
2014-05-19 02:27:00 -0400

    Fixed #86: changed default delimiter
    The default delimiter is now the pipe symbol, with spaces around it. I
    also fixed the formatting of the code itself.
    
    With that, this issue should be finished.

--- include/ScheduleWriter.h
+++ include/ScheduleWriter.h
@@ -8,16 +8,19 @@
 using std::ostream;
 using std::string;
 
-class ScheduleWriter : public FileWriter<Schedule*> {
+class ScheduleWriter : public FileWriter<Schedule*>
+{
 public:
-    ScheduleWriter(string filename, string delimiter = "\t") : FileWriter(filename) , _delimiter(delimiter) { };
+    ScheduleWriter(string filename, string delimiter = " | ") :
+        FileWriter(filename) , _delimiter(delimiter) { };
     void setFieldDelimiter(const string delimiter);
     void setContents(Schedule *contents);
     void write();
 private:
     std::string _delimiter;
     Schedule *_contents;
-    void printCourseLine(ostream& f, const string&, const string&, const string&, const string&, const string&, const string&, const string&);
+    void printCourseLine(ostream& f, const string&, const string&, const string&,
+            const string&, const string&, const string&, const string&);
     /** "Ellipsizes" string, so it won't go past a specific length. */
     string ellipsize(const string&, unsigned);
 };
