da391ddfe7012e3446676659f2959e208a346d7b
spygavin007 <spygavin007@gmail.com>
2014-05-19 00:36:11 -0400

    Update ScheduleWriter.h
    ostream is better, polymorphism with both cout (tests) and a file stream

--- include/ScheduleWriter.h
+++ include/ScheduleWriter.h
@@ -15,7 +15,7 @@ public:
 private:
     std::string _delimiter;
     Schedule *_contents;
-    void printCourseLine(ofstream& f, const string&, const string&, const string&, const string&, const string&, const string&, const string&);
+    void printCourseLine(ostream& f, const string&, const string&, const string&, const string&, const string&, const string&, const string&);
     /** "Ellipsizes" string, so it won't go past a specific length. */
     string ellipsize(const string&, unsigned);
 };
