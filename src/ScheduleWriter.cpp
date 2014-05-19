30b9556120142c22aa2c0ee4d9c91f1b3955c606
Karl Giesing <karl.giesing001@umb.edu>
2014-05-19 04:37:17 -0400

    Fixed #89
    When I was accounting for zero indexing, I was considering "pos" to be
    the position of the last character. But this is not the `substr`
    function takes; it takes a length. Forgot to set it back, resulting in
    edge cases where the ellipsed string is one character too long. Fixed.

--- src/ScheduleWriter.cpp
+++ src/ScheduleWriter.cpp
@@ -22,7 +22,7 @@ void ScheduleWriter::write() {
 	ofstream file;
 
 	file.open(this->getFilename().c_str());
-	
+
 	//key.
 	file << "CE - Course Enrollment" << endl << "RC - Room Capacity" << endl << endl;
 
@@ -33,25 +33,25 @@ void ScheduleWriter::write() {
 
 			file << w << " at " << t << ":\n\n";
 			vector<Course> courses = _contents->getCoursesAt(w, t);
-			
+
 			//header
 			printCourseLine(file, _delimiter, "Course ID", "Course Name", "Room", "Professor", "CE", "RC");
-			
+
 			//breaker line.
 			file << setfill('-');
 			printCourseLine(file, _delimiter, "-", "-", "-", "-", "-", "-");
 			file << setfill(' ');
-			
+
 			//the output.
-			
+
 			for(unsigned i = 0; i < courses.size(); i++) {
 				Room r = _contents->getRoomFor(courses[i]);
 				Prof p = _contents->getProf(courses[i]);
-				
+
 				stringstream enrolled, capacity;
 				enrolled << courses[i].getEnrolled();
 				capacity << r.getCapacity();
-				
+
 				printCourseLine(file, _delimiter, courses[i].getId(), courses[i].getName(),
 					r.getId(), p.getLastName(), enrolled.str(), capacity.str());
 			}
@@ -68,7 +68,7 @@ string ScheduleWriter::ellipsize(const string& input, unsigned maxLength)
     // Sanity check
     if (input.length() <= maxLength || maxLength < 3)
         return input;
-    unsigned pos = maxLength - 2; // Account for 3-char ellipsis, 0-index
+    unsigned pos = maxLength - 3; // Account for 3-char ellipsis
     // Find the last word boundary before the max length
     while (input[pos] != ' ' && pos > 0)
         pos--;
@@ -77,7 +77,7 @@ string ScheduleWriter::ellipsize(const string& input, unsigned maxLength)
         pos--;
     // If position is zero, put ellipsis in the middle of the word
     if (pos == 0)
-        pos = maxLength - 2;
+        pos = maxLength - 3;
     // Return the substring, with the ellipsis appended
     return input.substr(0, pos) + "...";
 }
@@ -85,7 +85,7 @@ string ScheduleWriter::ellipsize(const string& input, unsigned maxLength)
 void ScheduleWriter::printCourseLine(ostream& f, const string& _delimiter, const string& courseID,
 	const string& courseName, const string& room, const string& prof, const string& enrollment,
 	const string& capacity) {
-	
+
 	// delimiter is best " | ", 3 characters.
 	f << left << setw(14) << ellipsize(courseID, 14) << _delimiter
 	<< setw(25) << ellipsize(courseName, 25) << _delimiter
